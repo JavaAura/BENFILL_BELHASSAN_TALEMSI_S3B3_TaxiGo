84b5a1e68a05bd923f74269aa051418cad4413c6
Anass Benfillous <anasse.benfillous@gmail.com>
2024-11-12 13:52:38 +0100

    hadnle not found exception

--- TaxiGo/src/main/java/io/benfill/TaxiGo/exception/ExceptionMessage.java
+++ TaxiGo/src/main/java/io/benfill/TaxiGo/exception/ExceptionMessage.java
@@ -1,10 +1,10 @@
 package io.benfill.TaxiGo.exception;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
 
-public @Data class ExceptionMessage {
-
+@AllArgsConstructor
+public class ExceptionMessage {
 	private int status;
 	private String message;
-
+	
 }
