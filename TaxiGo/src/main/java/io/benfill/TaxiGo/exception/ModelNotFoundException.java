24428e8a0192a8c883ba9be2f7bb6e498f37d218
Anass Benfillous <anasse.benfillous@gmail.com>
2024-11-12 14:48:21 +0100

    update exception

--- TaxiGo/src/main/java/io/benfill/TaxiGo/exception/ModelNotFoundException.java
+++ TaxiGo/src/main/java/io/benfill/TaxiGo/exception/ModelNotFoundException.java
@@ -1,5 +1,9 @@
 package io.benfill.TaxiGo.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ModelNotFoundException extends RuntimeException {
 	 private String message;
 	
