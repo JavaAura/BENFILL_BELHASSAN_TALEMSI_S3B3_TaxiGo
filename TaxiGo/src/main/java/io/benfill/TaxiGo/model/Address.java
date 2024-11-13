6bbc85fc16950c306b4e189d087f27c6e4943fe8
Anass Benfillous <anasse.benfillous@gmail.com>
2024-11-13 11:44:01 +0100

    fix address converter

--- TaxiGo/src/main/java/io/benfill/TaxiGo/model/Address.java
+++ TaxiGo/src/main/java/io/benfill/TaxiGo/model/Address.java
@@ -2,8 +2,10 @@ package io.benfill.TaxiGo.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 public @Data class Address {
 
 	private String city;
